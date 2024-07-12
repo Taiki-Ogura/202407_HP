1e1082dbbc0d2f6f5d6330d2e26c43cef401287b
ogura taiki <ogura@camping-car.co.jp>
2024-07-12 21:46:27 +0900

    ハンバーガーメニューの追加

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".header-hamburger");
+  const nav = document.querySelector("nav");
+
+  hamburger.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
+
+  // ナビゲーションリンクをクリックしたときもメニューを閉じる
+  const navLinks = document.querySelectorAll("nav ul li");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      nav.classList.remove("show");
+    });
+  });
+
+  // メニュー外をクリックしたときにメニューを閉じる
+  document.addEventListener("click", function (event) {
+    const isClickInsideNav = nav.contains(event.target);
+    const isClickInsideHamburger = hamburger.contains(event.target);
+    if (!isClickInsideNav && !isClickInsideHamburger) {
+      nav.classList.remove("show");
+    }
+  });
+});
